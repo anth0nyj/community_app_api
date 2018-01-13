71b12f19ac6b483aef4c707975133b4ccb7eaee4
Anthony <ajtalvarez@gmail.com>
2018-01-13 16:46:24 -0500

    Syncing devdev and auth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authenticate_token, except: [:login, :create]
+  before_action :authorize_user, except: [:login, :create, :index]
 
   # GET /users
   def index
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -38,14 +40,41 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def login
+    user = User.find_by(username: params[:user][:username])
+
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.username)
+      render json: { status: 200, token: token, user: user }
+    else
+      render json: { status: 401, message: "Unauthorized" }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
+    def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+
+    def payload(id, username)
+      {
+        exp: (Time.now + 44640.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          username: username
+        }
+      }
+    end
+
     def set_user
       @user = User.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password)
+      params.require(:user).permit(:username, :password_digest)
     end
 end
