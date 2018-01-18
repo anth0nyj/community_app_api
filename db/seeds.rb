6e72e3bd5188a91cf1268ac35f342f08c39afebf
Josefina Mancilla <josefinanoemi4@gmail.com>
2018-01-17 18:04:21 -0600

    Updated seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,26 @@
 User.create(
-  [{username: 'spaceghost', password: 'spcaeghost'},
+  [{username: 'spaceghost', password: 'spaceghost'},
   {username: 'gg', password: 'gg'}]
 )
 
 Community.create(
-  [{name: 'GA WDIR Spaceghost', num_of_member: 5},
-  {name: 'Dog parents', num_of_member: 3},]
+  [{name: 'WDIR Spaceghost', num_of_member: 2}]
 )
 
 Ledger.create(
   [{user_id: 1, community_id: 1 },
   {user_id: 1, community_id: 2},
   {user_id: 2, community_id: 1},
-  {user_id: 2, community_id: 3},
-  {user_id: 2, community_id: 4},
-  {user_id: 3, community_id: 1},
-  {user_id: 3, community_id: 3},
-  {user_id: 3, community_id: 4},
-  {user_id: 4, community_id: 1},
-  {user_id: 4, community_id: 4},
-  {user_id: 5, community_id: 1},
-  {user_id: 5, community_id: 3},
-  {user_id: 5, community_id: 4},]
+  {user_id: 2, community_id: 2},
+]
 )
 
 Post.create(
-  [{title: 'This is a test post', content: 'Lorem ipsute wjaewoerw thiano waothonew thiwa mkaing up sstuff.. i hope whtis works', user_id: 1, community_id: 1 }]
+  [{title: 'Welcome to Spaceghost', content: "View our community's posts here.", user_id: 1, community_id: 1 }]
 )
 
 Reply.create(
-  [{content: 'Hector, you should get a dog', post_id: 4, user_id: 3}]
+  [{content: 'Connexus is a place that connects us. (Haha get it?)', post_id: 1, user_id: 2}]
 )
 
 p 'Seeded Data'
